48cb9f9b5361ea511ae69d47bb01d94ccfbcedf9
mori <armigofire@gmail.com>
2023-04-22 02:36:01 +0200

    base components and styles

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,8 +5,18 @@ type Props = {}
 
 export const About = (props: Props) => {
     return (
-        <div className="hall-main">
-            ABOUT
+        <div className="about-main">
+            <h1>about me</h1>
+            <p>
+                hii! i am an artist and developer from Barcelona. 
+                <br></br><br></br>
+                i mostly do 3D art and graphic design but i love all forms of art. i am a kinda noob developer but i am always learning and working on projects. sometimes i try to make games.
+                <br></br><br></br>
+                most of what i do is digital and i use a lot of software. my current goal is to be capable of modifying and improving the open source software that i use and cotribute to it.
+                <br></br><br></br>
+                i am also into web development. i made this portfolio myself :^).
+                
+            </p>
         </div>
     )
 }
--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Work.css"
+import Projects from "../Projects";
 
 type Props = {}
 
 export const Work = (props: Props) => {
     return (
-        <div className="about-main">
-            MY WORK
+        <div className="work-main">
+            <h1>my work</h1>
+            <Projects />
         </div>
     )
 }
