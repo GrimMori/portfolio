e3a6cec517efc608d046f9a5c8c5ddd9bda65983
mori <armigofire@gmail.com>
2023-09-03 19:44:18 +0200

    updated 'about me'

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,14 +8,11 @@ export const About = (props: Props) => {
         <div className="about-main">
             <h1>about me</h1>
             <p>
-                hii! i am an artist and developer from Barcelona. 
+                hii! i am an artist and developer from Barcelona.
                 <br></br><br></br>
-                i mostly do 3D art and graphic design but i love all forms of art. i am a kinda noob developer but i am always learning and working on projects. sometimes i try to make games.
+                i have worked on full stack projects using mainly Node.js, React, Nest.js, MongoDB and MySQL and i am looking for my first job as a developer. i also know Python and PostgreSQL.
                 <br></br><br></br>
-                most of what i do is digital and i use a lot of software. my current goal is to be capable of modifying and improving the open source software that i use and cotribute to it.
-                <br></br><br></br>
-                i am also into web development. i made this portfolio myself :^).
-                
+                i'm always working on something and trying to expand my skills and knownledge.
             </p>
         </div>
     )
